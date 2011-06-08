721142a5bbf982950eeb936a261cb10eab908b76
Gerhard Lipp <gerhard_lipp@gmx.de>
2011-06-08 18:27:14 +0200

    publish subscribe added

--- tango/zmq.lua
+++ tango/zmq.lua
@@ -4,6 +4,7 @@ local print = print
 local pcall = pcall
 local globals = _G
 
+--- A zmq (zeromq) compatible transport backend for tango.
 module('tango.zmq')
 
 
@@ -11,7 +12,6 @@ serve = function(zmq_url)
           local context = zmq.init(1)
           local socket = context:socket(zmq.REP)
           socket:bind(zmq_url or 'tcp://*:12345')
-          --socket:bind("ipc://horst")
           while true do            
             local request = socket:recv()
             local response = core.dispatch(request,globals,pcall)
@@ -39,3 +39,32 @@ client = function(zmq_url,call_type)
            }           
            return core.proxy(transport,call_type or core.call)
 end
+
+subscriber = function(zmq_url)
+          local context = zmq.init(1)
+          local socket = context:socket(zmq.SUB)
+          socket:connect(zmq_url or 'tcp://localhost:12346')
+          socket:setopt(zmq.SUBSCRIBE, "")
+          while true do            
+            local request = socket:recv()
+            core.dispatch(request,globals,pcall)
+          end
+          socket:close()
+          context:term()                       
+        end
+
+publisher = function(zmq_url)
+           local context = zmq.init(1)
+           local socket = context:socket(zmq.PUB)
+           socket:bind(zmq_url or 'tcp://*:12346')
+           local transport = {
+             context = context,
+             socket = socket,
+             send = 
+               function(tab)
+                 socket:send(tab)
+               end
+           }           
+           return core.proxy(transport,core.notify)
+end
+

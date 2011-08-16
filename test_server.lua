2c2d2fd0f8d910b5beadab14d2cd1bf15aafca7d
Gerhard Lipp <gelipp@googlemail.com>
2011-08-16 23:14:52 +0200

    reorganized

--- test_server.lua
+++ test_server.lua
@@ -1,9 +1,4 @@
--- sample server
-require'tango'
-
-local backend = arg[1] or 'copas'
-
-serve = require('tango.'..backend).serve
+local backend = arg[1]
 
 add = function(a,b)
          return a+b
@@ -21,18 +16,11 @@ customerror = function(err)
                error(err)
             end
 
-msleep = function(msec)           
-           local sleepcmd = 'sleep 0.'..string.format('%03d',msec)
-           print(sleepcmd)
-           io.popen(sleepcmd):read()           
-        end
-
 nested = {
   method = {
     name = function()return true end
   }
 }
 
-
--- starts the server on some default 'socket'
-serve()
+local backend = require('tango.server.'..backend)
+backend.loop()

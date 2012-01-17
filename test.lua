fc468ecdc2409608acf56ca9784dc8d989e9e74e
Gerhard Lipp <gerhard_lipp@gmx.de>
2012-01-17 11:11:26 +0100

    run ssl test only if ssl is available

--- test.lua
+++ test.lua
@@ -1,11 +1,14 @@
 #!/usr/bin/lua
 local run_client_test = 
-   function(server_backend,client_backend)
-     os.execute('lua test_client.lua '..server_backend..' '..client_backend)
+   function(server_backend,client_backend,option)
+     local cmd = 'lua test_client.lua %s %s %s'
+     os.execute(cmd:format(server_backend,client_backend,option or ''))
    end
 
 run_client_test('copas_socket','socket')
-run_client_test('copas_socket','socket','ssl')
+if pcall(require,'ssl') then
+  run_client_test('copas_socket','socket','ssl')
+end
 run_client_test('ev_socket','socket')
 run_client_test('zmq','zmq')
 
